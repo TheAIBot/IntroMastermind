8faf8d20721822b1c17b1bbc3d95a360e3085336
TheAIBot <andreasuranus@hotmail.com>
2019-09-23 21:50:04 +0200

    forgot using

--- C#/MasterMindAI/Program.cs
+++ C#/MasterMindAI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using IntrinsicAlgo;
 
 namespace MasterMindAI
 {
